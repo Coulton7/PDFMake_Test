70a09230ab96a5ceaa13c6666c60dc07e22b127d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-04 14:09:38 +0000

    figured out what was causing the errors now for test fire

--- js/pdfcreator.js
+++ js/pdfcreator.js
@@ -8,6 +8,7 @@ var docDefinition = {
     {text:'The health of the Earth is essential to not just business, but also species survival.', style: 'subheader'},
     'The Health, Safety and Welfare of all the stakeholders in our Group is the primary concern of the Board of AES Engineering Ltd.',
     'The Group has consistently put sustainability projects first for capital investment and has now decided to debate and publish a policy to prevent global warming through technological change and investment decisions.',
+    {
     ul: [
       'Any sustainability project will be given priority over any other capital investment with a similar Return on Investment.',
       'Any sustainability project with a reasonable chance of getting a Return on Investment of 4 years or less, should be brought to the attention of the AES Engineering Ltd. Board',
@@ -16,6 +17,7 @@ var docDefinition = {
       'As a Board we undertake to use our personal and corporate presence to influence policy makers to legislate the requirement for such a policy for all businesses with more than 250 co-workers.',
       'Current or potential supplier input is welcomed, including their use of the global hotline where necessary.'
     ],
+  },
     'The Board of AES Engineering Ltd. encourages the use of the global “hotline” on 0800-374199 or by email on aessealhotline@expolink.co.uk to assist with the discreet implementation of this policy where necessary.'
   ],
 
@@ -34,4 +36,4 @@ var docDefinition = {
 };
 
 var pdf = pdfmake.createPdf(docDefinition).download();
-};
+}
