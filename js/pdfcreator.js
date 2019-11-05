c9a5e642180560da1add836032549d29bb3f32bf
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-05 15:07:08 +0000

    Edited the script to have the new values in the js

--- js/pdfcreator.js
+++ js/pdfcreator.js
@@ -1,6 +1,9 @@
     var orgName = document.getElementById('orgName');
     var orgPhone = document.getElementById('orgPhone');
     var orgEmail = document.getElementById('orgEmail');
+    var orgWorkers = document.getElementById('orgWorkers');
+    var orgType = document.getElementById('orgType');
+    var roi = document.getElementById('roi');
 
     var docDefinition = {
 
@@ -21,15 +24,19 @@
         },
         {
           text: [
-            'The Health, Safety and Welfare of all the stakeholders in our Group is the primary concern of the Board of ', orgName.value, '.\n\n'
+            'The Health, Safety and Welfare of all the stakeholders in our', orgType.value, 'is the primary concern of the Board of ', orgName.value, '.\n\n'
           ]
         },
-        'The Group has consistently put sustainability projects first for capital investment and has now decided to debate and publish a policy to prevent global warming through technological change and investment decisions.\n\n',
+        {
+          text: [
+          'The', orgType.value, 'has consistently put sustainability projects first for capital investment and has now decided to debate and publish a policy to prevent global warming through technological change and investment decisions.\n\n',
+        ]
+      },
         {
           ul: [
             'Any sustainability project will be given priority over any other capital investment with a similar Return on Investment.\n\n',
             {
-              text: ['Any sustainability project with a reasonable chance of getting a Return on Investment of 4 years or less, should be brought to the attention of the ', orgName.value, '.\n\n']
+              text: ['Any sustainability project with a reasonable chance of getting a Return on Investment of', roi.value, 'years or less, should be brought to the attention of the ', orgName.value, '.\n\n']
             },
             'A written decision on any such project is mandatory within 3 months of project submission.\n\n',
             {
